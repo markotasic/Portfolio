a8f221323cba9491305f14535b03b286d6d8fb5f
markotasic <m4rko2001@gmail.com>
2022-11-20 17:31:46 +0100

    feat: add project

--- src/components/ProjectData/ProjectsData.tsx
+++ src/components/ProjectData/ProjectsData.tsx
@@ -2,6 +2,7 @@ import experienceTheWorld from '../../images/experience-the-world.jpg';
 import nishNocu from '../../images/nish-nocu.jpg';
 import iConnect from '../../images/iConnect.jpg';
 import eCommerce from '../../images/eCommerce.jpg';
+import passwordGenerator from '../../images/password-generator.jpg';
 import {
   CssIcon,
   HtmlIcon,
@@ -12,6 +13,7 @@ import {
   MUIIcon,
   MongoDBIcon,
   NodeJSIcon,
+  TSIcon,
 } from '../icons/index';
 
 export type projectItem = {
@@ -25,6 +27,20 @@ export type projectItem = {
 };
 
 export const ProjectData: projectItem[] = [
+  {
+    link: 'passwordGenerator',
+    title: 'Password Generator',
+    image: passwordGenerator,
+    live: 'https://marko-password-generator.netlify.app/',
+    code: 'https://github.com/markotasic/password-generator',
+    language: (
+      <div>
+        <ReactIcon />
+        <TSIcon />
+        <SassIcon />
+      </div>
+    ),
+  },
   {
     link: 'eCommerce',
     title: 'Sneakers',
